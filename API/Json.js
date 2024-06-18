0bc8820a4b96d007bca3c976128fb8689e5ad485
bilolganiev6 <152278651+bilolganiev6@users.noreply.github.com>
2024-06-18 09:33:35 +0300

    2
    2

--- API/Json.js
+++
@@ -1,7 +0,0 @@
-let jsonString = '{"name": "John", "age": 30, "city": "New York"}';
-let parsedData = JSON.parse(jsonString);
-console.log(parsedData.name);  // Вывод: John
-
-let jsObject = {name: "John", age: 30, city: "New York"};
-let jsonData = JSON.stringify(jsObject);
-console.log(jsonData);  // Вывод: {"name":"John","age":30,"city":"New York"}
